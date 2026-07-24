6b4c57f5ea50bad5d5a38b7ef736b8d447d51899
Selam47 <abdulselam4763@gmail.com>
2026-07-24 15:48:53 +0300

    fix: clean open-next config format

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,18 @@
-export default {
+const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
     },
   },
+  edgeExternals: ["pg-cloudflare"],
   middleware: {
-    external: ["pg-cloudflare"],
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+    },
   },
 };
+
+export default config;
