9cb7f0b35ee06a739cc371b965b2640e2b130ee2
Selam47 <abdulselam4763@gmail.com>
2026-07-24 15:59:05 +0300

    fix: explicit open-next config structure

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,18 +1,3 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-    },
-  },
-  edgeExternals: ["pg-cloudflare"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-    },
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default config;
+export default defineCloudflareConfig();
